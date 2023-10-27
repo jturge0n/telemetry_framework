6e662280072d94af64dd13011849d1aa82b3fcc0
Joshua Turgeon <jturgeon88@gmail.com>
2023-10-27 08:26:30 -0500

    init commit - initial skeleton

---
+++ lib/telemetry_monitor.rb
@@ -0,0 +1,19 @@
+def start_process(executable_path, command = "")
+
+end
+
+def create_file(file_path)
+
+end
+
+def modify_file(file_path, new_content)
+
+end
+
+def delete_file(file_path)
+
+end
+
+def establish_network_connection(destination, port, data)
+
+end
